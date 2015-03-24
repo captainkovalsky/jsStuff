9a1a6159e7916f1d2abb3d9b21808ca212cc61a0
Roman <victor.dzundza.dev@gmail.com>
2015-03-25 01:19:49 +0300

    first obstacle

--- orm/active-record.js
+++ orm/active-record.js
@@ -1,34 +1,44 @@
 /*** ACTIVE RECORD ***/
+(function () {
+    'use strict';
 
 
-function Object() {}
+    function Object() {}
 
-function DataTransport(type) {
-    this.type = type;
-}
-
-DataTransport.prototype.SEND_TYPE = {
-    JSON: 0,
-    BINARY: 1
-};
-
-DataTransport.prototype.sendJson = function (toSendJson) {
-    console.log('send json: ', toSendJson);
-}
-
-DataTransport.prototype.send = function () {
-    //detect type and choice best method for sending data
-    switch (this.type) {
-    case this.SEND_TYPE.JSON:
-        this.sendJson(this.data);
-        break;
-    default:
-        new Error('Unknown data type.');
+    function DataTransport(type) {
+        this.type = type;
     }
-}
+
+    DataTransport.prototype.SEND_TYPE = {
+        JSON: undefined,
+        BINARY: undefined
+    };
+
+    DataTransport.prototype.sendJson = function (toSendJson) {
+        console.log('send json: ', toSendJson);
+    }
+
+    DataTransport.prototype.send = function () {
+        //detect type and choice best method for sending data
+        switch (this.type) {
+        case this.SEND_TYPE.JSON:
+            this.sendJson(this.data);
+            break;
+        default:
+            new Error('Unknown data type.');
+        }
+    }
 
 
-Object.prototype.save = function (dataTransport) {}; //interface ??
+    Object.prototype.save = function (dataTransport) {
+        if (dataTransport.SEND_TYPE.JSON === dataTransport.type) {
+            dataTransport.send(JSON.stringify(this));
+        }
+    }; //interface ??
 
-var one = new Object(); //abstract example
-one.save(dataTranport);
+    var one = new Object(); //abstract example
+    one.save(dataTranport);
+
+
+
+})();
