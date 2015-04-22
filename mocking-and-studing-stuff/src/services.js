7560c81a5e57f159df7257ebaf46464a8df7a63c
Viktor Dzundza <Viktor_Dzundza@epam.com>
2015-04-22 23:42:41 +0300

    updated ignored files, done service logic more complicated

--- mocking-and-studing-stuff/src/services.js
+++ mocking-and-studing-stuff/src/services.js
@@ -1,22 +1,27 @@
 /**
  * Created by vdzundza on 22.04.15.
  */
-var testModule = angular.module('testModule');
+var testModule = angular.module( 'testModule' );
 
-testModule.service('serviceDep1', function serviceDepOne(){
-   this.getSomething = function(){
-      return 'serviceDep1';
-   };
-});
+testModule.service( 'serviceDep1', function serviceDepOne() {
+    this.getSomething = function ( value ) {
 
-testModule.service('serviceDep2', [
-    'serviceDep1', function serviceDepSecond(serviceOne){
-      this.doStuff = function(){
-         var serviceOneStuff = serviceOne.getSomething();
-         return 'Do my stuff and do ' + serviceOneStuff + ' serviceOne stuff';
-      };
-   }
-]);
+        if ( value >= 0 ) {
+            return 'A';
+        } else {
+            return 'B';
+        }
+    };
+} );
+
+testModule.service( 'serviceDep2', [
+    'serviceDep1', function serviceDepSecond( serviceOne ) {
+        this.doStuff = function ( value ) {
+            var serviceOneStuff = serviceOne.getSomething( value );
+            return 'Do my stuff and do ' + serviceOneStuff + ' serviceOne stuff';
+        };
+    }
+] );
 
 
 
