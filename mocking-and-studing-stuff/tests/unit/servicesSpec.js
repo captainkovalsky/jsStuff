9889f7c0c37c8810c3e024613fc7116e3eba18ee
Viktor Dzundza <Viktor_Dzundza@epam.com>
2015-04-23 01:32:28 +0300

    mock dependency

--- mocking-and-studing-stuff/tests/unit/servicesSpec.js
+++ mocking-and-studing-stuff/tests/unit/servicesSpec.js
@@ -4,21 +4,33 @@
 describe( 'service with dependencies', function () {
 
     var service2;
+    var getSomethingSpy;
+    var mockServiceDep1 = {
+        getSomething: function () {
+            return 'C';
+        }
+    };
+
     beforeEach( module( 'testModule' ) );
+
+    beforeEach( module( function ( $provide ) {
+        $provide.value( 'serviceDep1', mockServiceDep1 );
+    } ) );
 
     beforeEach( inject( function ( _serviceDep2_ ) {
         service2 = _serviceDep2_;
     } ) );
 
+
     it( 'should call service2 method', function () {
-       var service2Value = service2.doStuff();
-        expect(service2Value.length ).toEqual(2);
+        var service2Value = service2.doStuff();
+        expect( service2Value.length ).toEqual( 2 );
     } );
 
-    it('should start with A', function(){
+    it( 'should start with A', function () {
         var service2Value = service2.doStuff();
-        expect(service2Value[0]).toEqual('A'); //@todo: want to manage what dep1 returns 'A' or 'B' and test this behaviour
-    });
+        expect( service2Value[ 0 ] ).toEqual( 'C' ); //@todo: want to manage what dep1 returns 'A' or 'B' and test this behaviour
+    } );
 
     it( 'check test', function () {
         expect( 2 ).toBe( 2 );
