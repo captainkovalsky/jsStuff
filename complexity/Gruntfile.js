2dcca510244a296f3fa7433ba25a7ecb9677de02
vdzundza <victor.dzundza.dev@gmail.com>
2015-05-25 15:41:13 +0300

    Complexity check with grunt file

---
+++ complexity/Gruntfile.js
@@ -0,0 +1,26 @@
+ // Project configuration.
+ module.exports = function(grunt) {
+     grunt.initConfig({
+         complexity: {
+             generic: {
+                 src: ['src/*.js'],
+                 exclude: ['doNotTest.js'],
+                 options: {
+                     breakOnErrors: true,
+                     // jsLintXML: 'report.xml', // create XML JSLint-like report
+                     // checkstyleXML: 'checkstyle.xml', // create checkstyle report
+                     // pmdXML: 'pmd.xml', // create pmd report
+                     errorsOnly: false, // show only maintainability errors
+                     cyclomatic: [3, 7, 12], // or optionally a single value, like 3
+                     halstead: [8, 13, 20], // or optionally a single value, like 8
+                     maintainability: 100,
+                     hideComplexFunctions: false, // only display maintainability
+                     broadcast: false // broadcast data over event-bus
+                 }
+             }
+         }
+     });
+
+     grunt.loadNpmTasks('grunt-complexity');
+     grunt.registerTask('default', 'complexity');
+ };
