68eaba23be4875d2f8dbf39b9052b55b2c647de2
Viktor Dzundza <victor.dzundza.dev@gmail.com>
2015-07-09 01:26:36 +0300

    disable prod building

--- babel-angular-filter-oop/gulpfile.js
+++ babel-angular-filter-oop/gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('watch', function(){
 gulp.task("js", function () {
   return gulp.src("src/**/*.js")
     .pipe(sourcemaps.init())
-    .pipe(concat("app.js"))
+    // .pipe(concat("app.js"))
     .pipe(babel())
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("public/app"));
