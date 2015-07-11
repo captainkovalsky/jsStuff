2c6c88c5715c214dd07ad090f17b42e442db93d7
Viktor Dzundza <victor.dzundza.dev@gmail.com>
2015-07-12 02:35:18 +0300

    added koa

--- alphabet/app/js/controllers/example.js
+++ alphabet/app/js/controllers/example.js
@@ -13,6 +13,10 @@ function ExampleCtrl() {
   vm.title = 'AngularJS, Gulp, and Browserify!';
   vm.number = 1234;
 
+  vm.loadCriminals = function(){
+    console.log('load criminals');
+  };
+
 }
 
 controllersModule.controller('ExampleCtrl', ExampleCtrl);
